eefeaa6a82f680951a27fc508497bdd2e157e030
JayLaks <jayshree_lakshman@yahoo.com>
2019-02-13 22:07:08 +0000

    All changes after Third Day

--- src/app/features/todos-redux/components/list/list.component.ts
+++ src/app/features/todos-redux/components/list/list.component.ts
@@ -1,3 +1,20 @@
+// import { Component, Input, OnInit } from '@angular/core';
+// import { TodoListItem } from '../../models';
+
+// @Component({
+//   selector: 'app-list',
+//   templateUrl: './list.component.html',
+//   styleUrls: ['./list.component.css']
+// })
+// export class ListComponent implements OnInit {
+
+//   @Input() list: TodoListItem[];
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
 import { Component, Input, OnInit } from '@angular/core';
 import { TodoListItem } from '../../models';
 
@@ -12,5 +29,4 @@ export class ListComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
